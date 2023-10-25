74be54045edafd9bc41e5f59981f16ac1dc42ede
Simas <71006658+simuxakadiscgolfgod@users.noreply.github.com>
2023-10-25 13:04:46 +0300

    Create 2-3.sql

---
+++ Big-Query-Tasks/Basic-SQL/2-3.sql
@@ -0,0 +1,7 @@
+SELECT																									
+wor.WorkOrderID,																									
+SUM(wor.ActualCost) AS Total_Actual_Cost																									
+FROM `tc-da-1.adwentureworks_db.workorderrouting` AS wor																									
+WHERE DATE_TRUNC(EXTRACT(DATE FROM wor.ActualStartDate), MONTH) = '2004-01-01'																									
+GROUP BY wor.WorkOrderID																									
+HAVING Total_Actual_Cost > 300;
