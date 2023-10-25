7ff2f38f4798bc487160c8ec94bbaf8af02534e0
Simas <71006658+simuxakadiscgolfgod@users.noreply.github.com>
2023-10-25 13:27:43 +0300

    Update 2-1.sql

--- Big-Query-Tasks/Basic-SQL/2-1.sql
+++ Big-Query-Tasks/Basic-SQL/2-1.sql
@@ -1,9 +1,9 @@
-SELECT																									
-LocationID,																									
-COUNT(DISTINCT WorkOrderID) AS Num_Work_Orders,																									
-COUNT(DISTINCT ProductID) AS Num_Unique_Products,																									
-SUM(ActualCost) AS Total_Actual_Cost																									
-FROM `tc-da-1.adwentureworks_db.workorderrouting`																									
-WHERE DATE_TRUNC(EXTRACT(DATE FROM ActualStartDate), MONTH) = '2004-01-01'																									
-GROUP BY LocationID																									
+SELECT
+LocationID,
+COUNT(DISTINCT WorkOrderID) AS Num_Work_Orders,
+COUNT(DISTINCT ProductID) AS Num_Unique_Products,
+SUM(ActualCost) AS Total_Actual_Cost
+FROM `tc-da-1.adwentureworks_db.workorderrouting`
+WHERE DATE_TRUNC(EXTRACT(DATE FROM ActualStartDate), MONTH) = '2004-01-01'
+GROUP BY LocationID
 ORDER BY Total_Actual_Cost DESC;
